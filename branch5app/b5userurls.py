4c9285ab6d3bf57247eaefd6ddd9efb087cc4385
brightgeorge <98938996+brightgeorge@users.noreply.github.com>
2023-06-13 22:26:28 +0530

    up1306202317

--- branch5app/b5userurls.py
+++ branch5app/b5userurls.py
@@ -146,6 +146,7 @@ path('dec_make_payments_advance5/<id>', branch5.dec_make_payments_advance5, name
 
     path('may_unpaid_rent5/', branch5.may_unpaid_rent5, name='may_unpaid_rent5'),
     path('june_unpaid_rent5/', branch5.june_unpaid_rent5, name='june_unpaid_rent5'),
+    path('table_june_unpaid_rent5/', branch5.table_june_unpaid_rent5, name='table_june_unpaid_rent5'),
     path('july_unpaid_rent5/', branch5.july_unpaid_rent5, name='july_unpaid_rent5'),
     path('aug_unpaid_rent5/', branch5.aug_unpaid_rent5, name='aug_unpaid_rent5'),
 
